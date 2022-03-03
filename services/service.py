99e001a36ba9afa55a62356a8a78a804908337ab
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-03-03 11:30:01 +0100

    build get stock service

---
+++ services/service.py
@@ -0,0 +1,10 @@
+import imp
+from flask_restful import fields, marshal_with, abort
+
+from repository.db import DB
+
+class Service:
+
+    @staticmethod
+    def get_stock():
+        return DB.get_stock()
