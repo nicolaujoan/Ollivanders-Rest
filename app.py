e779e152c443ce2ae8be3d16e7569a04512f6e97
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-03-08 16:53:07 +0100

    feat: add resources as endpoints in the api

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 from controllers.item import Item
 from controllers.welcome import Welcome;
 from controllers.stock import Stock
+from controllers.quality import StockByQuality
+from controllers.sell_in import StockBySellIn
 
 # app instance
 app = Flask(__name__)
@@ -14,6 +16,8 @@ api = Api(app, catch_all_404s=True)
 api.add_resource(Welcome, '/')
 api.add_resource(Item, '/item/<name>')
 api.add_resource(Stock, '/stock')
+api.add_resource(StockBySellIn, '/stock/sell-in')
+api.add_resource(StockByQuality, '/stock/quality')
 
 # REPOSITORY (INIT OUR SQLITE3 DB)
 from repository.sql import dbRel
