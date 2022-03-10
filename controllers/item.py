af1ea0e9bd47919eef03eec167c63a88c9f49041
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-03-10 09:57:48 +0100

    refactor route and move post to item resource

--- controllers/item.py
+++ controllers/item.py
@@ -5,5 +5,8 @@ class Item(Resource):
     def get(self, name):
         return Service.get_item(name),200
     
+    def post(self, name):
+        return Service.post_item(name)
+    
     def delete(self, name):
         return Service.delete_item(name), 200
