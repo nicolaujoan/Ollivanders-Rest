25edd496d013048b977805c7bea49ad8c9f0b26f
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-03-13 20:34:54 +0100

    add logic to obtain desired output in the requests

--- services/service.py
+++ services/service.py
@@ -1,6 +1,4 @@
 from flask_restful import abort
-
-# from repository.dbMock import DB
 from repository.sql import dbRel
 
 class Service:
@@ -9,25 +7,65 @@ class Service:
     def get_stock():
         return dbRel.stock()
     
+
     @staticmethod
     def get_stock_by_sell_in():
         return dbRel.stock_by_sell_in()
     
+
     @staticmethod
     def get_stock_by_quality():
         return dbRel.stock_by_quality()
     
+
+    # this converts one item into a 'json doc'
+    @staticmethod
+    def __format_one_item(item):
+        desired_item = {
+            'id': 0,
+            'name': "",
+            'sell_in': 0,
+            'quality': 0
+        }
+
+        desired_item['id'] = item[0][0]
+        desired_item['name'] = item[0][1]
+        desired_item['sell_in'] = item[0][2]
+        desired_item['quality'] = item[0][3]
+        
+        return desired_item
+    
+
+    # this one converts the output into a json doc or an array of json docs
+    @staticmethod
+    def __formated(item):
+
+        if len(item) == 1:
+            item = Service.__format_one_item(item)
+            return item
+        
+        if len(item) > 1:
+            items = [Service.__format_one_item([i]) for i in item]
+            return items
+        return item
+    
+
     @staticmethod
     def get_item(name):
         item = dbRel.item(name)
         if not item:
             abort(404, message="item {} not in stock".format(name))
-        return dbRel.item(name)
+        else:
+            item = Service.__formated(item)
+        return item
     
+
     @staticmethod
     def post_item(name):
         dbRel.post_item(name, sell_in=10, quality=10)
     
+
     @staticmethod
     def delete_item(name):
-        dbRel.delete_item_by_name(name)
+        deleted_id = dbRel.delete_item_by_name(name)
+        return {'id': deleted_id}
