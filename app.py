03c7d715a73e09f3d0b39a30ef85107dcb030609
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-03-20 11:17:32 +0100

    refactor: create_app function to setup tests and able to select testing or production db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from resources.factory import create_app
+from flask import Flask
 from flask_restful import Api
 from resources.item import Item
 from resources.welcome import Welcome;
@@ -6,19 +6,31 @@ from resources.stock import Stock
 from resources.quality import StockByQuality
 from resources.sell_in import StockBySellIn
 
-# app instance (production or test mode)
-app = create_app(production=False)  # need to create a config file maybe for testing purposes or to put the route of the db
+def create_app(production=False):
+    # app instance (production or test mode)
+    app = Flask(__name__)
 
-# REST API
-api = Api(app, catch_all_404s=True)
+    # if app.config.db = sqlite
+    # take the sqlite db
+    from repository.sql import dbRel
+    if production:
+        dbRel.init_app(app)
+    else:
+        dbRel.init_app(app, False)
 
-# RESOURCES
-api.add_resource(Welcome, '/')
-api.add_resource(Item, '/item/<name>')
-api.add_resource(Stock, '/stock')
-api.add_resource(StockBySellIn, '/stock/sell-in')
-api.add_resource(StockByQuality, '/stock/quality')
+    # REST API
+    api = Api(app, catch_all_404s=True)
+
+    # RESOURCES
+    api.add_resource(Welcome, '/')
+    api.add_resource(Item, '/item/<name>')
+    api.add_resource(Stock, '/stock')
+    api.add_resource(StockBySellIn, '/stock/sell-in')
+    api.add_resource(StockByQuality, '/stock/quality')
+
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
