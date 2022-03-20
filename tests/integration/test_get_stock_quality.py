5a01bc26cf4969d5a621238fe266e8f2ca48d253
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-03-20 13:16:32 +0100

    test: pass tests of filtering stock by sellin and quality

---
+++ tests/integration/test_get_stock_quality.py
@@ -0,0 +1,70 @@
+from repository.sql.dbRel import init_db, inserts_db
+
+def test_get_stock_by_quality(app, client):
+
+    expected_stock = [
+    {
+        "id": 4,
+        "itsname": "Sulfuras, Hand of Ragnaros",
+        "sell_in": 0,
+        "quality": 80
+    },
+    {
+        "id": 5,
+        "itsname": "Sulfuras, Hand of Ragnaros",
+        "sell_in": -1,
+        "quality": 80
+    },
+    {
+        "id": 7,
+        "itsname": "Backstage passes to a TAFKAL80ETC concert",
+        "sell_in": 10,
+        "quality": 49
+    },
+    {
+        "id": 8,
+        "itsname": "Backstage passes to a TAFKAL80ETC concert",
+        "sell_in": 5,
+        "quality": 49
+    },
+    {
+        "id": 2,
+        "itsname": "+5 Dexterity Vest",
+        "sell_in": 10,
+        "quality": 20
+    },
+    {
+        "id": 6,
+        "itsname": "Backstage passes to a TAFKAL80ETC concert",
+        "sell_in": 15,
+        "quality": 20
+    },
+    {
+        "id": 3,
+        "itsname": "Elixir of the Mongoose",
+        "sell_in": 5,
+        "quality": 7
+    },
+    {
+        "id": 9,
+        "itsname": "Conjured Mana Cake",
+        "sell_in": 3,
+        "quality": 6
+    },
+    {
+        "id": 1,
+        "itsname": "Aged Brie",
+        "sell_in": 2,
+        "quality": 0
+    }
+    ]
+
+    with app.app_context():
+        init_db()
+        inserts_db()
+    
+    resp = client.get('/stock/quality')
+    assert resp.status_code == 200
+    data = resp.json
+    assert isinstance(data, list)
+    assert data == expected_stock
