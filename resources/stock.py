a887db51ba5bc0ba65f39f2b26ecbe5dd796ad82
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-03-20 19:29:44 +0100

    feat: add put resource to update quality

--- resources/stock.py
+++ resources/stock.py
@@ -2,6 +2,11 @@ from flask_restful import Resource
 from services.service import Service
 
 class Stock(Resource):
-    # curl http://localhost:8000/stock
+    # curl http://localhost:5000/stock
     def get(self):
         return Service.get_stock(), 200
+    
+    # put == update
+    # curl http://localhost:5000/stock -X PUT -v
+    def put(self):
+        return Service.update_quality(), 201
